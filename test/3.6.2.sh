89b446493705760e37909046a16bbefeb3a49f47
Matt Stofko <778087+c0sco@users.noreply.github.com>
2019-01-11 11:44:57 -0800

    Don't pass -q to egrep when piping output to egrep

--- test/3.6.2.sh
+++ test/3.6.2.sh
@@ -3,6 +3,6 @@
 
 # 3.6.2 - Ensure default deny firewall policy (Scored)
 
-iptables -L | egrep -q "Chain[[:space:]]+INPUT[[:space:]]+" | egrep -q "policy[[:space:]]+DROP" || exit 1
-iptables -L | egrep -q "Chain[[:space:]]+FORWARD[[:space:]]+" | egrep -q "policy[[:space:]]+DROP" || exit 1
-iptables -L | egrep -q "Chain[[:space:]]+OUTPUT[[:space:]]+" | egrep -q "policy[[:space:]]+DROP" || exit 1
+iptables -L | egrep "Chain[[:space:]]+INPUT[[:space:]]+" | egrep -q "policy[[:space:]]+DROP" || exit 1
+iptables -L | egrep "Chain[[:space:]]+FORWARD[[:space:]]+" | egrep -q "policy[[:space:]]+DROP" || exit 1
+iptables -L | egrep "Chain[[:space:]]+OUTPUT[[:space:]]+" | egrep -q "policy[[:space:]]+DROP" || exit 1
