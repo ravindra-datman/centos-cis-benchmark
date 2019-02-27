b2399e371224c7464b2cfc717281330c19c5aa4a
Brandon Danielski <brandon.danielski@act-on.net>
2019-02-27 14:32:34 -0800

    Brandon's Initial push for filter test and test remediation

--- test/5.3.1.sh
+++ test/5.3.1.sh
@@ -3,6 +3,13 @@
 
 # 5.3.1 - Ensure password creation requirements are configured (Scored)
 
-grep pam_cracklib.so /etc/pam.d/password-auth | grep -E "try_first_pass\s*retry=3\s*minlen=14" || exit $1
+grep pam_pwquality.so /etc/pam.d/password-auth | grep -E "try_first_pass\s*retry=3" || exit $1
 
-grep pam_cracklib.so /etc/pam.d/system-auth | grep -E "try_first_pass\s*retry=3\s*minlen=14" || exit $1
+grep pam_pwquality.so /etc/pam.d/system-auth | grep -E "try_first_pass\s*retry=3" || exit $1
+
+grep "minlen = 14" /etc/security/pwquality.conf || exit $1
+grep "dcredit = -1" /etc/security/pwquality.conf || exit $1
+grep "ucredit = -1" /etc/security/pwquality.conf || exit $1
+grep "ocredit = -1" /etc/security/pwquality.conf || exit $1
+grep "lcredit = -1" /etc/security/pwquality.conf || exit $1
+
