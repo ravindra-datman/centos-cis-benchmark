0877ba7239569a9b0c0f5bfa9ff6305c4fbdcbe8
Brandon Danielski <brandon.danielski@act-on.net>
2019-03-29 11:14:29 -0700

    Test update

--- test/4.2.1.4.sh
+++ test/4.2.1.4.sh
@@ -5,7 +5,7 @@
 # 4.2.1.4 "Ensure rsyslog is configured to send logs to a remote log host (Scored)" Yes Server1 Workstation1
 
 execute(){
-grep -q "^\*.\*[^I][^I]*@" /etc/rsyslog.conf 2>/dev/null || return 1
+grep -q "^\*.\*[^I][^I]*@" /etc/rsyslog.d/* /etc/rsyslog.conf 2>/dev/null || return 1
 }
 test_serial_number="4.2.1.4"
 test_name="Ensure rsyslog is configured to send logs to a remote log host (Scored)"
